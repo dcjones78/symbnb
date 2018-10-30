bcfe7b3c461d52212a425483853a3e445eed2b7d
sebastien <sj.jacquet@gmail.com>
2018-10-30 20:49:04 +0100

    Session 18

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,22 +4,37 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class HomeController{
+class HomeController extends Controller {
+
+    /**
+     * Montre la page qui dit bonjour
+     *
+     * @Route("/bonjour/{prenom}/age/{age}", name="hello")
+     * @Route("/salut", name="hello_base")
+     * @Route("/bonjour/prenom", name="hello_prenom")
+     */
+    public function hello($prenom = "anonyme", $age = 0){
+
+        return new Response("Bonjour " . $prenom . " vous avez " . $age . " ans");        
+    }
 
     /**
      * @Route("/", name="homepage")
     */
-    public function homme(){
+    public function home(){
 
-        return new Response("<html>
-                                <head>
-                                    <title>Mon application</title>
-                                </head>
-                                <body>
-                                    <h1>Bonjour a tous</h1>
-                                </body>
-                            </html>");
+        $prenoms = ["sebastien" => 31, "sabine" => 12, "luka" => 54, "noemie" => 26];
+
+        return $this->render(
+            'home.html.twig',
+            [
+                'title' => 'Aurevoir tous monde',
+                'age'   => 12,
+                'tableau' => $prenoms    
+            ]
+        );
     }
 }
 ?>
