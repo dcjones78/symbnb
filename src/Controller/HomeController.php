2c21b8192741ebc9cee3496285a9690930ccaa49
sebastien <sj.jacquet@gmail.com>
2018-11-01 18:58:59 +0100

    Session 23

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -17,7 +17,13 @@ class HomeController extends Controller {
      */
     public function hello($prenom = "anonyme", $age = 0){
 
-        return new Response("Bonjour " . $prenom . " vous avez " . $age . " ans");        
+        //return new Response("Bonjour " . $prenom . " vous avez " . $age . " ans");        
+        return $this->render("hello.html.twig", 
+            [
+                'prenom' => $prenom,
+                'age' => $age
+            ]
+            );
     }
 
 
