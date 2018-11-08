25f4bd5c8d2282b7bf8aa8006ea3a39531d548d9
sebastien <sj.jacquet@gmail.com>
2018-11-08 20:02:31 +0100

    Session 37

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Repository\AdRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdController extends AbstractController
@@ -11,10 +13,10 @@ class AdController extends AbstractController
     /**
      * @Route("/ads", name="ads_index")
      */
-    public function index()
+    public function index(AdRepository $repo, SessionInterface $session)
     {
-        $repo = $this->getDoctrine()->getRepository(Ad::class);
-
+        //$repo = $this->getDoctrine()->getRepository(Ad::class);
+        dump($session);
         $ads = $repo->findAll();
 
         return $this->render('ad/index.html.twig', [
