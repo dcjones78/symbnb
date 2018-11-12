143a6451ffd40174adc8ee07fc1f867dcb574b36
sebastien <sj.jacquet@gmail.com>
2018-11-12 21:11:08 +0100

    Session 43

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Ad;
 use App\Repository\AdRepository;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,14 +14,33 @@ class AdController extends AbstractController
     /**
      * @Route("/ads", name="ads_index")
      */
-    public function index(AdRepository $repo, SessionInterface $session)
+    public function index(AdRepository $repo)
     {
         //$repo = $this->getDoctrine()->getRepository(Ad::class);
-        dump($session);
+    
         $ads = $repo->findAll();
 
         return $this->render('ad/index.html.twig', [
             'ads' => $ads,
+        ]);
+    }
+
+    /**
+     * permet d'afficher une seule annonce
+     * 
+     *@Route("/ads/{slug}", name="ads_show")
+     * @return Response
+     */
+    public function show($slug, AdRepository $repo){
+
+        //die($slug);
+        // je recupere une annonce qui correspond au slug
+        $ad = $repo->findOneBySlug($slug);
+
+        //die($ad->getTitle());
+
+        return $this->render("ad/show.html.twig", [
+            'ad' => $ad
         ]);
     }
 }
